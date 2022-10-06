5241ca21fa51285d65d6207000cfd0c539fcde5b
Haxu <nguyenngocminh190901@gmail.com>
2022-10-06 20:05:39 +0700

    create a AudioPlayer schema

---
+++ src/app/models/AudioPlayer.js
@@ -0,0 +1,57 @@
+const { string } = require('joi');
+const Joi = require('joi');
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const audioPlayerSchema = new mongoose.Schema(
+    {
+        owner: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+        currentPlayingTrack: { type: String },
+        description: { type: String, default: '' },
+        queue: [
+            {
+                track: { type: String, required: true },
+                album: { type: String, required: true },
+                addedAt: { type: Date, default: Date.now() },
+                _id: false,
+            },
+        ],
+        tracks: [
+            {
+                track: { type: String, required: true },
+                album: { type: String, required: true },
+                addedAt: { type: Date, default: Date.now() },
+                _id: false,
+            },
+        ],
+        context: {
+            type: { type: String },
+            currentPlaylist: { type: String },
+            currentAlbum: { type: String },
+            currentLiked: { type: String },
+            currentArtist: { type: String },
+        },
+        isRepeat: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+        isShuffle: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+        volume: {
+            type: Number,
+            required: true,
+            default: 50,
+            min: 0,
+            max: 100,
+        },
+    },
+    { timestamps: true },
+);
+
+const AudioPlayer = mongoose.model('AudioPlayer', audioPlayerSchema);
+
+module.exports = { AudioPlayer };
