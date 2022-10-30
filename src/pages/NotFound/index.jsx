c845388d24223426d323a80d5429c40b92b72c35
Haxu <nguyenngocminh190901@gmail.com>
2022-10-30 12:54:08 +0700

    create not found and unauthorized page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,20 +1,38 @@
 import { Link, useNavigate } from 'react-router-dom';
+import classNames from 'classnames/bind';
+
+import record from '~/assets/images/record.svg';
+import recordArm from '~/assets/images/record-arm.svg';
+import styles from './styles.scoped.scss';
+import CustomButton from '~/components/CustomButton';
+import { useTranslation } from 'react-i18next';
+
+const cx = classNames.bind(styles);
 
 const NotFound = () => {
+    const { t } = useTranslation();
     const navigate = useNavigate();
 
     const goBack = () => navigate(-1);
+
     return (
-        <article style={{ padding: '100px' }}>
-            <h1>Oops!</h1>
-            <p>Page Not Found</p>
-            <div>
-                <Link to='/'>Visit Our Homepage</Link>
+        <div className={cx('container')}>
+            <div className='left'>
+                <div className={cx('info')}>
+                    <h1 className={cx('title')}>404 Not Found</h1>
+                    <p className={cx('text')}>
+                        {t("We couldn't find the page you were looking for. Maybe our FAQ or Community can help?")}
+                    </p>
+                </div>
+                <div className={cx('navigation')}>
+                    <CustomButton onClick={() => goBack()}>{t('Go Back')}</CustomButton>
+                </div>
             </div>
-            <div>
-                <button onClick={goBack}>Go Back</button>
+            <div className={cx('right')}>
+                <img src={record} alt='record' className={cx('record')} />
+                <img src={recordArm} alt='record-arm' className={cx('record-arm')} />
             </div>
-        </article>
+        </div>
     );
 };
 
--- src/pages/UnAuthorized/index.jsx
+++ src/pages/UnAuthorized/index.jsx
@@ -1,19 +1,39 @@
 import { useNavigate } from 'react-router-dom';
+import classNames from 'classnames/bind';
+
+import record from '~/assets/images/record.svg';
+import recordArm from '~/assets/images/record-arm.svg';
+import styles from './styles.scoped.scss';
+import CustomButton from '~/components/CustomButton';
+import { useTranslation } from 'react-i18next';
+
+const cx = classNames.bind(styles);
 
 const UnAuthorized = () => {
+    const { t } = useTranslation();
     const navigate = useNavigate();
 
     const goBack = () => navigate(-1);
 
     return (
-        <section>
-            <h1>UnAuthorized</h1>
-            <br />
-            <p>You do not have access to the requested page.</p>
-            <div>
-                <button onClick={goBack}>Go Back</button>
+        <div className={cx('container')}>
+            <div className='left'>
+                <div className={cx('info')}>
+                    <h1 className={cx('title')}>403 Unauthorized</h1>
+                    <p className={cx('text')}>
+                        {t("Either you aren't cool enough to visit this page or it doesn't exist...")}
+                        <em>{t('like your social life.')}</em>
+                    </p>
+                </div>
+                <div className={cx('navigation')}>
+                    <CustomButton onClick={() => goBack()}>Go Back</CustomButton>
+                </div>
             </div>
-        </section>
+            <div className={cx('right')}>
+                <img src={record} alt='record' className={cx('record')} />
+                <img src={recordArm} alt='record-arm' className={cx('record-arm')} />
+            </div>
+        </div>
     );
 };
 
