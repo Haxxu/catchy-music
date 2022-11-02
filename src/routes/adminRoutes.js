29c3d3ffad09d719c12c78c8b09a76e47f43a5fb
Haxu <nguyenngocminh190901@gmail.com>
2022-11-02 21:05:46 +0700

    create structure for profile page and settings page in admin dashboard

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -4,8 +4,22 @@ import AdminDashboardLayout from '~/layouts/AdminDashboardLayout';
 // Pages
 import Dashboard from '~/pages/admindashboard/Dashboard';
 import ManageUser from '~/pages/admindashboard/ManageUser';
+import Profile from '~/pages/Profile';
+import Settings from '~/pages/Settings';
 
 const adminRoutes = [
+    {
+        path: routes.admin_dashboard_profile,
+        component: Profile,
+        layout: AdminDashboardLayout,
+        roles: [roles.admin],
+    },
+    {
+        path: routes.admin_dashboard_settings,
+        component: Settings,
+        layout: AdminDashboardLayout,
+        roles: [roles.admin],
+    },
     // dashboard
     {
         path: routes.admin_dashboard,
