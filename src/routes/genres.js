4ec2cf58d59e55fc9a79ed05e6fcbd788aede83c
Haxu <nguyenngocminh190901@gmail.com>
2022-12-02 11:48:23 +0700

    get playlist, artists, albums in getGenreById

--- src/routes/genres.js
+++ src/routes/genres.js
@@ -6,12 +6,6 @@ const adminAuth = require('../app/middlewares/adminAuth');
 const validateObjectId = require('../app/middlewares/validateObjectId');
 const artistAuth = require('../app/middlewares/artistAuth');
 
-// [GET] /api/genres => get all genres
-router.get('/', genreController.getAllGenres);
-
-// [POST] /api/genres/ => create genre
-router.post('/', adminAuth, genreController.createGenre);
-
 // [GET] /api/genres/:id => get genre by id
 router.get('/:id', [userAuth, validateObjectId], genreController.getGenreById);
 
@@ -20,5 +14,11 @@ router.put('/:id', [adminAuth, validateObjectId], genreController.updateGenre);
 
 // [DELETE] /api/genres/:id => delete genre
 router.delete('/:id', [adminAuth, validateObjectId], genreController.deleteGenre);
+
+// [GET] /api/genres => get all genres
+router.get('/', genreController.getAllGenres);
+
+// [POST] /api/genres/ => create genre
+router.post('/', adminAuth, genreController.createGenre);
 
 module.exports = router;
