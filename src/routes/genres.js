cce49189889cbc0985f73cbf09a285ef9aaf2be3
Haxu <nguyenngocminh190901@gmail.com>
2022-12-07 19:18:19 +0700

    format line code routes

--- src/routes/genres.js
+++ src/routes/genres.js
@@ -8,16 +8,12 @@ const artistAuth = require('../app/middlewares/artistAuth');
 
 // [GET] /api/genres/:id => get genre by id
 router.get('/:id', [userAuth, validateObjectId], genreController.getGenreById);
-
 // [PUT] /api/genres/:id => update genre
 router.put('/:id', [adminAuth, validateObjectId], genreController.updateGenre);
-
 // [DELETE] /api/genres/:id => delete genre
 router.delete('/:id', [adminAuth, validateObjectId], genreController.deleteGenre);
-
 // [GET] /api/genres => get all genres
 router.get('/', genreController.getAllGenres);
-
 // [POST] /api/genres/ => create genre
 router.post('/', adminAuth, genreController.createGenre);
 
--- src/routes/tracks.js
+++ src/routes/tracks.js
@@ -9,22 +9,16 @@ const playTrackLimiter = require('../app/middlewares/playTrackLimiter');
 
 // [POST] /api/tracks => create new track
 router.post('/', artistAuth, trackController.createTrack);
-
 // [GET] /api/tracks/?search= => Get tracks by context
 router.get('/context', adminAuth, trackController.getTracksByContext);
-
 // [GET] /api/tracks/info => get tracks info
 router.get('/info', adminAuth, trackController.getTracksInfo);
-
 // [GET] /api/tracks/:id/lyrics => get lyrics of track
 router.get('/:id/lyrics', [userAuth, validateObjectId], trackController.getLyricsOfTrack);
-
 // [GET] /api/tracks/:id => get track by id
 router.get('/:id', [userAuth, validateObjectId], trackController.getTrackById);
-
 // [PUT] /api/tracks/:id => update track by id
 router.put('/:id', [artistAuth, validateObjectId], trackController.updateTrack);
-
 // [DELETE] /api/tracks/:id => remove track by id
 router.delete('/:id', [artistAuth, validateObjectId], trackController.deleteTrack);
 
